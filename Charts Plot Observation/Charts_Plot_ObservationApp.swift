311e24ff13d4794d7c2bf0721f2a0ffaff48fc8a
jterry94 <terryj@iit.edu>
2024-01-15 17:17:46 -0600

    Initial Commit

---
+++ Charts Plot Observation/Charts_Plot_ObservationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Charts_Plot_ObservationApp.swift
+//  Charts Plot Observation
+//
+//  Created by Jeff_Terry on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Charts_Plot_ObservationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Charts Plot ObservationUITests/Charts_Plot_ObservationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Charts_Plot_ObservationUITestsLaunchTests.swift
+//  Charts Plot ObservationUITests
+//
+//  Created by Jeff_Terry on 1/15/24.
+//
+
+import XCTest
+
+final class Charts_Plot_ObservationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
